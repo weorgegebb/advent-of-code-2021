701d5ae43be4d2028a13a381a4fb526cb431406b
George Webb <george.d.webb@live.co.uk>
2021-12-18 16:21:06 +0000

    Update demo

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ Fill this in!
 Demo                 | Just a test
 Day 1                | Sonar Sweep
 Day 2                | Dive!
+Day 3                | Binary Diagnostic
--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,7 +1,42 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+namespace {
+auto CheckInputIsValid(const int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << "usage: " << argv[0] << " path-to-input" << std::endl;
+    return false;
+  }
+  return true;
+}
+
+auto GetInputData(const char* path) {
+  std::ifstream input_file(path);
+  std::string line;
+  std::vector<std::string> output_lines;
+
+  while (std::getline(input_file, line)) {
+    if (line.size() > 0) {
+      output_lines.push_back(line);
+    }
+  }
+  return output_lines;
+}
+}  // namespace
+
+auto main(int argc, char* argv[]) -> int {
+  if (!CheckInputIsValid(argc, argv)) {
+    return 0;
+  }
+
+  const auto& file_path = argv[1];
+  const auto report = GetInputData(file_path);
+
+  // Part 1
+
+  // Part 2
+
   return 0;
 }
